791a304b3ca7c94d8b858c88706164ee61b8a2a9
Jordan Jacobelli <jordan@cri.epita.net>
2017-10-18 15:14:12 +0200

    dump: Add option to specify disk

--- states/files/dump/home/root/dump.sh
+++ states/files/dump/home/root/dump.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 IMAGE_NAME=$(cat /proc/cmdline | sed "s/.*dump_image=\([^ ]*\).*/\1/")
+DISK_NAME=$(cat /proc/cmdline | sed "s/.*dump_disk=\([^ ]*\).*/\1/")
 NFS_SERVER="clone-store-1.pie.cri.epita.net"
 NFS_DIR="/srv/nfs"
 
 [[ -z "IMAGE_NAME" ]] && echo "Error: Empty image name" && exit 1
+[[ -z "DISK_NAME" ]] && echo "Error: Empty disk name" && exit 1
 
 echo "Dumping image $IMAGE_NAME"
 
@@ -16,6 +18,6 @@ mount -t nfs4 -o ro $NFS_SERVER:$NFS_DIR /home/partimag
 echo "Mount done"
 echo "Start dump"
 
-ocs-sr -g auto -e1 auto -e2 -r -j2 -scr -p choose restoredisk $IMAGE_NAME sda
+ocs-sr -g auto -e1 auto -e2 -r -j2 -scr -p choose restoredisk $IMAGE_NAME $DISK_NAME
 
 echo "Done"
