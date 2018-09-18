652a6d55814a31988e55530ba4bd7f63421166ae
Marin Hannache <mareo@cri.epita.net>
2018-09-18 23:25:17 +0200

    exec-archlinux: fix typo in exec_url parameter name
    Signed-off-by: Marin Hannache <mareo@cri.epita.net>

--- states/files/exec-archlinux/home/root/exec.sh
+++ states/files/exec-archlinux/home/root/exec.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-EXEC_URL=$(cat /proc/cmdline | sed "s/.*exec-url=\([^ ]*\).*/\1/")
+EXEC_URL=$(cat /proc/cmdline | sed "s/.*exec_url=\([^ ]*\).*/\1/")
 wget "${EXEC_URL}" -O /tmp/script.sh
 chmod +x /tmp/script.sh
 /tmp/script.sh
